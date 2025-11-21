d93a94616b92cb9e1cfd4d9e1a651dc2235b4dc7
tadeulps <tadeslopes@gmail.com>
2025-11-20 21:43:40 -0300

    feat: add caixa-button component; implement customizable button with variants and icon support

---
+++ src/app/components/caixa-button/caixa-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-caixa-button',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './caixa-button.component.html',
+  styleUrls: ['./caixa-button.component.scss']
+})
+export class CaixaButtonComponent {
+  @Input() variant: 'primary' | 'secondary' | 'outlined' = 'primary';
+  @Input() disabled: boolean = false;
+  @Input() fullWidth: boolean = false;
+  @Input() icon?: string;
+  @Output() clicked = new EventEmitter<void>();
+
+  onClick(): void {
+    if (!this.disabled) {
+      this.clicked.emit();
+    }
+  }
+}
